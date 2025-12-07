4c8a3a78986f6a450b4cd30cf6b19b18074dd11a
pyi27 <pyi.htun@live.com>
2025-12-07 01:30:26 +0000

    fix path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ccc-v2/', 
+  base: '/',  // ✅ root path for custom domain
   plugins: [react()],
   build: { outDir: 'docs' }
 })
